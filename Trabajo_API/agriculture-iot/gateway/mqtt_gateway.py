de476980e5f6c221f9f12ed929f91bafa8188842
Miguel Bercedo <162442989+Almigam@users.noreply.github.com>
2025-12-11 11:10:03 +0100

    Add files via upload

--- Trabajo_API/agriculture-iot/gateway/mqtt_gateway.py
+++ Trabajo_API/agriculture-iot/gateway/mqtt_gateway.py
@@ -12,7 +12,7 @@ import paho.mqtt.client as mqtt
 import json
 import requests
 import os
-import logging
+import logging 
 from datetime import datetime
 from typing import Dict, Any
 
@@ -36,4 +36,59 @@ class MQTTGateway:
         self.client = mqtt.Client(client_id="mqtt_gateway")
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        pass
+        
+        self.api_token = None
+        self.sensor_readings = {}
+    
+    def authenticate_api(self) -> bool:
+        """Auntenticar con la API y obtener JWT"""
+        try:
+            response = requests.post(
+                f"{API_URL}/auth/login",
+                data={
+                    "username": API_USERNAME,
+                    "password": API_PASSWORD
+                }, 
+                timeout=10
+            )
+            if response.status_code == 200:
+                data = response.json()
+                self.api_token = data["access_token"]
+                logger.info("Authenticated with Asset API")
+                return True
+            else:
+                logger.error(f"Authentication failed: {response.status_code}")
+                return False
+        except Exception as e:
+            logger.error(f"API authentication error: {e}")
+            return False
+        
+    def on_connect(self, client, rc):
+        """un callback para cuando se conecta con el broker de MQTT"""
+        if rc == 0:
+            logger.info(f"Connected to MQTT Broker: {MQTT_BROKER}")
+            #El cliente tiene que subscribirse a todos los topics de los sensores.
+            client.subscribe("agriculture/sensors/#")
+            logger.info("Subscribed to: agriculture/sensors/#")
+        else:
+            logger.error(f"Connection failed with code {rc}")
+
+    def on_message(self, msg):
+        """Callback para cuando se recibe mensaje MQTT"""
+        try:
+            #Parsear el payload del JSON
+            payload = json.loads(msg.payload.decode('utf-8'))
+            logger.info(f"Received from {msg.topic}")
+            logger.debug(f"Payload: {json.dumps(payload, indent=2)}")
+            #Procesar JSON según tipo sensor
+            self.process_sensor_data(msg.topic, payload)
+
+        except json.JSONDecodeError as e:
+            logger.error(f"Invalid JSON {e}")
+        except Exception as e:
+            logger.error(f"Error processing message: {e}", exc_info=True)
+    
+    def process_sensor_data(self, topic: str, payload: Dict[str, Any]):
+        """Procesa los datos del sensor y envía"""
+
+        #QUEDA: TERMINAR ESTA FUNCIÓN Y AÑADIR CHECKEOS Y CREAR LOS ASSETS PARA CADA SENSOR
